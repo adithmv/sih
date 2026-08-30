75318dc2db936a6bda8b14d6bebb32d908ad24a5
adithmv <adithmvs3@gmail.com>
2026-08-30 20:12:51 +0530

    Fix clinic flows and demo configuration

---
+++ src/lib/facilities.js
@@ -0,0 +1,9 @@
+export const facilities = [
+  { id: "FAC-1001", name: "Govt. Taluk Hospital, Kochi", abhaRegistered: true },
+  { id: "FAC-1002", name: "Govt. General Hospital, Ernakulam", abhaRegistered: true },
+  { id: "FAC-2001", name: "Lakeshore Hospital (Empanelled)", abhaRegistered: true },
+  { id: "FAC-2291", name: "City Care Small Clinic", abhaRegistered: false },
+  { id: "FAC-2300", name: "Green Valley Clinic", abhaRegistered: true },
+];
+
+export const defaultFacilityId = "FAC-2291";
